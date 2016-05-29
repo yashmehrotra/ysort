a43cd5ae7b0dfe453c4f58ba2d8df590392f7ef9
Yash Mehrotra <yashmehrotra95@gmail.com>
2016-05-29 13:35:48 +0530

    Converting into package

--- ysort.go
+++ ysort.go
@@ -1,30 +1,23 @@
-package main
+package ysort
 
 import (
-    "fmt"
     "time"
     "sync"
 )
 
-var wg sync.WaitGroup
-var ret_arr []int
 
-func YSort(arr []int) {
+func Sort(arr []int) []int {
+    var wg sync.WaitGroup
+    var ret_arr []int
     wg.Add(len(arr))
     for i := range arr {
         go func (i int) {
             defer wg.Done()
             time.Sleep(time.Duration(arr[i]) * time.Millisecond)
-            fmt.Println(arr[i])
             ret_arr = append(ret_arr, arr[i])
         }(i)
     }
     wg.Wait()
-    fmt.Println(ret_arr)
+    return ret_arr
 }
 
-
-func main() {
-    arr := []int{5, 2, 6, 3, 1, 4}
-    YSort(arr)
-}
