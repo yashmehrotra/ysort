776d8a6045194293614f805c395c02e0131423aa
Yash Mehrotra <yashmehrotra95@gmail.com>
2016-05-29 13:40:09 +0530

    Style Fix

--- ysort.go
+++ ysort.go
@@ -1,23 +1,21 @@
 package ysort
 
 import (
-    "time"
-    "sync"
+	"sync"
+	"time"
 )
 
-
 func Sort(arr []int) []int {
-    var wg sync.WaitGroup
-    var ret_arr []int
-    wg.Add(len(arr))
-    for i := range arr {
-        go func (i int) {
-            defer wg.Done()
-            time.Sleep(time.Duration(arr[i]) * time.Millisecond)
-            ret_arr = append(ret_arr, arr[i])
-        }(i)
-    }
-    wg.Wait()
-    return ret_arr
+	var wg sync.WaitGroup
+	var ret_arr []int
+	wg.Add(len(arr))
+	for i := range arr {
+		go func(i int) {
+			defer wg.Done()
+			time.Sleep(time.Duration(arr[i]) * time.Millisecond)
+			ret_arr = append(ret_arr, arr[i])
+		}(i)
+	}
+	wg.Wait()
+	return ret_arr
 }
-
